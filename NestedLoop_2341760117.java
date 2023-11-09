6447a8012db98a67b186fc9c3bac6d8b27e3c4df
LOUISENZR <louisanaza@gmail.com>
2023-11-09 10:08:38 +0700

    second commit

--- NestedLoop_2341760117.java
+++ NestedLoop_2341760117.java
@@ -13,7 +13,7 @@ public class NestedLoop_2341760117 {
         }
         for (int i = 0; i < temps.length; i++){
             System.out.println("Kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps.length; j++){
+            for (int j = 0; j < temps[0].length; j++){
                 System.out.print(temps[i][j] + " ");
             }
             System.out.println();
